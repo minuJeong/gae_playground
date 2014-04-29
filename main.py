6dbd2ed3b75db706adbfa070451c4bf745cd478d
minuj <minu.hanwool@gmail.com>
2014-04-29 19:00:12 +0900

    afdsafdsa

--- main.py
+++ main.py
@@ -15,28 +15,10 @@ JINJA = jinja2.Environment(
                             extensions=['jinja2.ext.autoescape'],
                             autoescape=True)
 
-class StringData() :
-    def __init__(self) :
-        self.stringData = markdown.markdown("# head #")
-
 class MainHandler(webapp2.RequestHandler) :
     arr_post = []
 
     def get(self) :
-
-        data = StringData()
-        self.response.write(data.stringData)
-        user = users.get_current_user()
-        if user :
-            greeting = "Welcome, %s (<a href=%s>sign out</a>)" % (user.nickname(), users.create_logout_url('/'))
-        else :
-            greeting = "<a href=%s>Sign in or register</a>" % (users.create_login_url('/'))
-
-        values = {
-            "greeting": greeting
-
-        }
-
         template = JINJA.get_template("index.html")
         self.response.write(template.render(values))
 
